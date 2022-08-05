357fc3503c3f953a05b409008c3e88587f6e0302
Daniel Dias Pereira <86617987+DanielDPereira@users.noreply.github.com>
2022-08-05 20:06:55 -0300

    Update math.js

--- math.js
+++ math.js
@@ -74,7 +74,7 @@ btn.addEventListener("click", function(e){
         document.getElementById("-D").innerHTML ="";
         document.getElementById("Resultado D").innerHTML ="<b>Delta = </b>"+delta;
         document.getElementById("Resultado X1").innerHTML ="<b>X1 = </b>"+X1;
-        document.getElementById("Resultado X2").innerHTML ="<b>X2 = </b>"+X2";
+        document.getElementById("Resultado X2").innerHTML ="<b>X2 = </b>"+X2;
     }
 
     //Retornando valores no HTML - fim
